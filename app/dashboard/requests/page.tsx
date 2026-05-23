4d54d3924d7e4d7c1f385a6fc64f59d83aa25961
Workbit <workbitsf@gmail.com>
2026-05-23 15:23:21 +0200

    fix: remove invalid request select field

--- app/dashboard/requests/page.tsx
+++ app/dashboard/requests/page.tsx
@@ -99,7 +99,6 @@ export default async function DashboardRequestsPage() {
             endTime: true,
           },
         },
-        certificateCode: true,
       },
     }),
     canCreateRequests
